7717c6f02cc261b00ee8d5b419b2ec01398d807f
Bruna Guarizo <brunaguarizo@Mac.lan>
2024-10-20 11:45:34 -0700

    historyLetConst

---
+++ history.js
@@ -0,0 +1,6 @@
+let warrior = 10;
+const wizard = "wizard";
+
+if(warrior === 10){ 
+    console.log("The " +wizard+ " is clever and great with tricks, when he looked at the mirror he could see a  "+warrior+ ".");
+}
